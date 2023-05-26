9e58de531e05be957f577a92f0b7586d83ab55b2
roud <roud255@163.com>
2023-05-27 01:44:32 +0800

    fix

--- vue/src/views/Index.vue
+++ vue/src/views/Index.vue
@@ -2,11 +2,16 @@
     <div style="position: relative;min-height:100vh;min-width: 758px;overflow: hidden;">
         <div class="bg-container">
         </div>
-        <el-page-header @back="goBack" style="color:#ffffff; font-size: 16px !important; font-weight: 200 !important; margin-left: 5%" title="返回首页">
-          <template #content>
-            <span style="font-size: 16px; color: #ffffff; font-weight: 200"> 登录&注册 </span>
-          </template>
-        </el-page-header>
+<!--        <el-page-header @back="goBack" style="color:#ffffff; font-size: 16px !important; font-weight: 200 !important; margin-left: 5%" title="返回首页">-->
+<!--          <template #content>-->
+<!--            <span style="font-size: 16px; color: #ffffff; font-weight: 200"> 登录&注册 </span>-->
+<!--          </template>-->
+<!--        </el-page-header>-->
+        <span style="display: inline-block;position: absolute; top:5%;left: 5%; height:16px;line-height:16px;vertical-align:middle; cursor: pointer" @click="goBack">
+          <el-icon color="white" style="float: left"><ArrowLeftBold /></el-icon>
+          <span style="display: inline-block; color: white; font-size: 16px; float: left">返回</span>
+        </span>
+
         <div style="position: absolute;
                     width: 480px;
                     height: 632px;
